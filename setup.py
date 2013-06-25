185e2baa9c720a929c514cc8d28246229a23f757
Alon Swartz <alon@turnkeylinux.org>
2013-06-25 19:23:19 +0300

    added Makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import octohub
+from distutils.core import setup
+
+setup(
+    name='octohub',
+    version=octohub.__version__,
+    description='Low level Python and CLI interface to GitHub',
+    author='Alon Swartz',
+    author_email='alon@turnkeylinux.org',
+    url='https://github.com/turnkeylinux/octohub',
+    packages=[
+        'octohub',
+    ],
+)
